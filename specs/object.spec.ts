7e0af8810e83fa041961d9fb333e06cd2e06f4c6
potapovDim <potapovd3012@gmail.com>
2020-08-25 14:32:52 +0300

    add objectIsNotEmpty objectIsEmpty

--- specs/object.spec.ts
+++ specs/object.spec.ts
@@ -4,14 +4,38 @@ import {AssertionError} from 'assert';
 
 
 describe('Object', function() {
-  it('[P] stringNotIncludesSubstring', function() {
+  it('[P] objectIncludesKeys', function() {
     expect({a: 2, b: 3, c: {a: 1, b: 2}}).objectIncludesKeys(['a', 'b']);
     expect({a: 2, b: 3, c: {a: 1, b: 2}}).objectIncludesKeys(['a', 'b', 'c']);
   });
 
-  it('[N] stringNotIncludesSubstring', function() {
+  it('[N] objectIncludesKeys', function() {
     try {
       expect({a: 1}).objectIncludesKeys(['c']);
+    } catch (error) {
+      assert.equal(error instanceof AssertionError, true);
+    }
+  });
+
+  it('[P] objectIsNotEmpty', function() {
+    expect({a: 1}).objectIsNotEmpty();
+  });
+
+  it('[N], objectIsNotEmpty', function() {
+    try {
+      expect({}).objectIsNotEmpty();
+    } catch (error) {
+      assert.equal(error instanceof AssertionError, true);
+    }
+  });
+
+  it('[P] objectIsEmpty', function() {
+    expect({}).objectIsEmpty();
+  });
+
+  it('[N], objectIsEmpty', function() {
+    try {
+      expect({a: 1}).objectIsNotEmpty();
     } catch (error) {
       assert.equal(error instanceof AssertionError, true);
     }
